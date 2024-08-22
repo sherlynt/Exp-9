a4918a21bdd660e61109a5857ba16a1aa7178225
sherlynt <tiwarisherlyn@gmail.com>
2024-08-22 20:44:26 +0530

    Create exp9-1.cpp

---
+++ exp9-1.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h> 
+using namespace std;
+void geeks()
+{
+    int var = 5;
+    int* ptr;                  
+    ptr = &var;
+
+    cout<<"Value at ptr = "<<ptr<<"\n";
+    cout<<"Value at var = " <<var<<"\n";
+    cout<<"Value at *ptr = "<<*ptr<<"\n";
+
+}
+
+int main()
+{
+    geeks();
+    return 0;
+}
