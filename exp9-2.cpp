271df6048cb00d877c5a6340081bca90d888d1c0
sherlynt <tiwarisherlyn@gmail.com>
2024-08-22 20:44:56 +0530

    Create exp9-2.cpp

---
+++ exp9-2.cpp
@@ -0,0 +1,23 @@
+#include <iostream> 
+using namespace std; 
+
+int main() 
+{
+    int* p=new int[7];  
+
+    for (int i=0; i<5; i++)  
+    {
+        p[i]=10*(i+1);
+    }
+
+    cout<<*p<<"\n"; 
+    cout<<*p+1<<"\n";
+    cout<<*(p+1)<<"\n";
+    cout<<2[p]<<"\n";
+    cout<<p[2]<<"\n";
+    *p++;
+
+    cout<<*p;                
+
+    return 0; 
+}
